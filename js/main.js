2ef3c4d1dbe63ca6ed901ca28ba01913fbbe2d00
leriil <leriil@mail.ru>
2019-01-23 22:10:19 +0300

    Conditional rendering

--- js/main.js
+++ js/main.js
@@ -1,72 +1,7 @@
 // here an instance of Vue is created
 new Vue({
-el:'#app',
-data:{
-	name: 'Max',
-	title: 'Hello!',
-	link: 'http://google.com',
-	anotherLink: '<a href="http://google.com">Google</a>',
-    counter: 0,
-	attachRed: false,
-	color: 'green',
-	width: 100 },
-    // are executed no matter what
-    //the result is not cached
-methods:{
-	changeTitle: function(event){
-		this.title=event.target.value;
-	},
-	// to get access to properties we use this keyword, 
-	// it doesn't work without it
-	sayGoodbye: function(){
-		// overriding the value of 'title'
-		this.title='Hello you';
-		return this.title;
-	},
-	increase: function(step){
-		this.counter+=step;
+	el: '#app',
+	data: {
+		show: true
 	}
-},
-// is used like a property not a function
-// are calculated only if the value of counter changes
-//it means that the result is cached
-// best practise to use them whenever possible
-// but only for synchronized tasks
-computed:{
-	output: function(){
-		return this.counter>5?'Greater than 5': 'Less than 5';
-
-	},
-	divClasses: function(){
-		return {
-			red: this.attachRed,
-			blue: !this.attachRed
-		}
-	},
-	myStyle: function(){
-		return {
-			backgroundColor: this.color,
-			width: this.width +'px'
-		};
-	}
-},
-// another object apart from data, methods etc.
-watch:{
-// the property name we want to watch
-//the code I want to execute when the value of this property changes
-// which is passed automatically to the function
-// no return value
-// (this references a Vuejs object here)
-	counter: function(value){
-		var vm = this;
-		// this is the reserved function
-		// the function inside is a callback function
-		setTimeout(function(){
-			vm.counter = 0;
-		},2000);
-
-	}
-
-
-}
 });
