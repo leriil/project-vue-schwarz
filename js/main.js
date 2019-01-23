ae32a3bda64b24b72b90ffcbedc2e0bc0d2f134f
Likhacheva Ekaterina <eklikhac@t-systems.ru>
2019-01-23 17:53:55 +0300

    watch object added which watches the changes of a data property

--- js/main.js
+++ js/main.js
@@ -27,10 +27,29 @@ methods:{
 // is used like a property not a function
 // are calculated only if the value of counter changes
 //it means that the result is cached
+// best practise to use them whenever possible
+// but only for synchronized tasks
 computed:{
 	output: function(){
 		return this.counter>5?'Greater than 5': 'Less than 5';
 
 	}
+},
+// another object apart from data, methods etc.
+watch:{
+// the property name we want to watch
+//the code I want to execute when the value of this property changes
+// which is passed automatically to the function
+// no return value
+// (this references a Vuejs object here)
+	counter: function(value){
+		var vm = this;
+		setTimeout(function(){
+			vm.counter = 0;
+		},2000);
+
+	}
+
+
 }
 });
